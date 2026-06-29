22a6a0cffd30f51723864ae60685a101a083cee9
Sandhya-0314 <sandhyalokesh2403@gmail.com>
2026-06-29 16:41:52 +0530

    fastapiapp

---
+++ frontend/talentspark/src/components/JobCard.tsx
@@ -0,0 +1,11 @@
+function JobCard(){
+    return(
+        <div>
+            <h1>Software Engineer</h1>
+            <p>Google </p>
+            <p>Bangalore</p>
+            <p>5LPA</p>
+        </div>
+    )
+}
+export default JobCard
